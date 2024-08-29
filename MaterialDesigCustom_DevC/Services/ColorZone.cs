b14abcc8f8090b443fc1ce5df32b175a2fda255e
DESKTOP-N61EGAN\Admin <phamxuancongnb99@gmail.com>
2024-08-30 06:57:02 +0700

    uop

---
+++ MaterialDesigCustom_DevC/Services/ColorZone.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MaterialDesigCustom_DevC.Services
+{
+    public class ColorZone : ContentControl
+    {
+        static ColorZone()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ColorZone), new FrameworkPropertyMetadata(typeof(ColorZone)));
+        }
+
+        public static readonly DependencyProperty ModeProperty = DependencyProperty.Register(
+            nameof(Mode), typeof(ColorZoneMode), typeof(ColorZone), new PropertyMetadata(default(ColorZoneMode)));
+
+        public ColorZoneMode Mode
+        {
+            get => (ColorZoneMode)GetValue(ModeProperty);
+            set => SetValue(ModeProperty, value);
+        }
+
+        public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.Register(
+            nameof(CornerRadius), typeof(CornerRadius), typeof(ColorZone), new PropertyMetadata(default(CornerRadius)));
+
+        public CornerRadius CornerRadius
+        {
+            get => (CornerRadius)GetValue(CornerRadiusProperty);
+            set => SetValue(CornerRadiusProperty, value);
+        }
+    }
+}
---
+++ MaterialDesigCustom_DevC/Services/ColorZoneMode.cs
@@ -0,0 +1,17 @@
+﻿namespace MaterialDesigCustom_DevC.Services
+{
+    public enum ColorZoneMode
+    {
+        Standard,
+        Inverted,
+        PrimaryLight,
+        PrimaryMid,
+        PrimaryDark,
+        SecondaryLight,
+        SecondaryMid,
+        SecondaryDark,
+        Light,
+        Dark,
+        Custom
+    }
+}
